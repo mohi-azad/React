3599807d2c1eb697229ddd1013ea2c2e3f2091c3
id23mad <id23mad@cs.umu.se>
2025-04-29 10:39:53 +0200

    new commit

--- reacthtml/vite.config.js
+++ reacthtml/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React/', // Lägg till ett inledande och avslutande snedstreck
+  base: '/React/', 
 });
